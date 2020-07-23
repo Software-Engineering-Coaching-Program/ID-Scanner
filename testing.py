30a9cc01c79c0c779921a27aa59c26f29614c27e
James Wei <james.wei.cs@gmail.com>
2020-07-23 06:05:44 -0700

    put text with params

--- testing.py
+++ testing.py
@@ -27,7 +27,7 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 #print(pytesseract.image_to_boxes(img))
 
 #take info of image
-himg,wImg,_ = img.shape
+hImg,wImg,_ = img.shape
 boxes = pytesseract.image_to_boxes(img)
 #acquiring the location for every character
 for b in boxes.splitlines():
@@ -38,7 +38,11 @@ for b in boxes.splitlines():
 
     x,y,w,h = int(b[1]),int(b[2]),int(b[3]),int(b[4])
     #how our rectangle will look. each will have rectangle
-    cv2.rectangle(img,(x,y),(w+x,h+y),(0,0,255),1)
+    #subtract y to shorten the amount
+    cv2.rectangle(img,(x,hImg-y),(w,hImg-h),(0,0,255),2)
+    #put text around box, b0 is first element of text 
+    #putText(img, text, org, fontFace, fontScale, color, thickness=None, lineType=None, bottomLeftOrigin=None
+    cv2.putText(img,b[0],(x,hImg-y+25),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
 
     
  
