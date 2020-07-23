e7a169a6f1c37b64319588a6217c45f38682aff8
James Wei <james.wei.cs@gmail.com>
2020-07-23 04:45:14 -0700

    using openCV

--- testing.py
+++ testing.py
@@ -1,7 +1,20 @@
-import pytesseract as tess 
-tess.pytesseract.tesseract_cmd = r'C:\Users\james\AppData\Local\Tesseract-OCR\tesseract'
-from PIL import Image
-img = Image.open('test.png')
-text = tess.image_to_string(img)
+#import numpy as np
+#import cv2
+#import pytesseract as tess 
+#tess.pytesseract.tesseract_cmd = r'C:\Users\james\AppData\Local\Tesseract-OCR\tesseract'
+#from PIL import Image
+#img = Image.open('test.png')
+#text = tess.image_to_string(img)
 
-print(text)
+#print(text)
+
+#import cv2
+#import pytesseract as tess 
+#tess.pytesseract.tesseract_cmd = r'C:\Users\james\AppData\Local\Tesseract-OCR\tesseract'
+
+
+import cv2
+import pytesseract
+ 
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\james\AppData\Local\Tesseract-OCR\tesseract'
+img = cv2.imread('opencv.png')
