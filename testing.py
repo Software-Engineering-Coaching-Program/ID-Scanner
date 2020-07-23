d6c2e59e5ae6f4f3d9ecd2056daf1146269b6054
James Wei <james.wei.cs@gmail.com>
2020-07-23 05:22:42 -0700

    read in image

--- testing.py
+++ testing.py
@@ -17,4 +17,12 @@ import cv2
 import pytesseract
  
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\james\AppData\Local\Tesseract-OCR\tesseract'
+ #read in img with file name 
 img = cv2.imread('opencv.png')
+
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+ 
+
+ 
+cv2.imshow('Result',img)
+cv2.waitKey(0)
